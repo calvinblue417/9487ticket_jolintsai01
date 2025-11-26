474cec728b0a416498ca07ee0ebabf5015903b4a
calvinblue417 <xji41438@gmail.com>
2025-11-27 06:35:30 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const TEST_MODE = false;
 
 // 2. 遊戲設定
 export const TARGET_DATE = "2025-11-30T20:00:00"; // 遊戲開始時間
-export const GITHUB_BASE_URL = "https://raw.githubusercontent.com/calvinblue417/9487ticket/main"; // 圖片素材路徑
+export const GITHUB_BASE_URL = ""; // 圖片素材路徑
 
 // 3. 題目答案設定 (CARDS)
 // 請在 answer 欄位填入正確的咒語答案
@@ -23,4 +23,4 @@ export const CARDS_CONFIG = [
 export const FINAL_ANSWER = "86e50149658661312a9e0b35558d84f6c6d3da797f552a9657fe0558ca40cdef";
 
 // 圖片路徑輔助函式
-export const getImg = (name: string) => `${GITHUB_BASE_URL}/${name}`;
+export const getImg = (name: string) => `/${name}`;
