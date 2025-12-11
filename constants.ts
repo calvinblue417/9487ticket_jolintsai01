22184afd2f2255f2a7de87015c13bfbe76c0a69a
calvinblue417 <xji41438@gmail.com>
2025-12-11 13:00:13 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -10,13 +10,13 @@ export const GITHUB_BASE_URL = ""; // 圖片素材路徑
 // 3. 題目答案設定 (CARDS)
 // 請在 answer 欄位填入正確的咒語答案
 export const CARDS_CONFIG = [
-  { id: 1, answer: "b74373ee429b49c01636ba65e7c4884be13807031bb46a29be3ad149c4a48af6" },
-  { id: 2, answer: "a565f049499290c29ae14e70e595d270389baceb8a5260c8691c19a5d0cf348b" },
-  { id: 3, answer: "59643d5dc0153442a5a9014d5b6c82cdcb6d53027fd78dacef527c7818901737" },
-  { id: 4, answer: "5725693a81c41bd1bb6a80477e9189ba938c7d1d05a8eaf5d500ddec5b63bef1" },
-  { id: 5, answer: "eba03aaf9113950b7eb4ae6f610281b2add7ec658ca08b96a0c27e4ab5fbf7d3" },
-  { id: 6, answer: "793a84a351bd364d2f0323b67b39407711e54bc4748c439fb32734538ef8dd15" },
-  { id: 7, answer: "c688fe9d72d2aa7634f4e503cd6cd6a56825e90bfa2bcef8e8b755586e2e493d" },
+  { id: 1, answer: "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" },
+  { id: 2, answer: "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" },
+  { id: 3, answer: "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" },
+  { id: 4, answer: "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" },
+  { id: 5, answer: "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" },
+  { id: 6, answer: "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" },
+  { id: 7, answer: "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" },
 ];
 
 // 4. 最終題目答案 (LIGHT 3)
